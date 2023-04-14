9f9a7069fd13acaceaa97ae9df158e504837d42f
dom <35171942+domluther@users.noreply.github.com>
2023-04-14 19:18:02 +0100

    calculates single operation

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // Store calculator state/model centrally. Ease of access
-const calculator = {
-  num1: 0,
-  num2: 0,
-  operator: null,
-  result: 0,
-  displayValue: "0",
-};
+const calculator = {};
 
 const calculatorEle = document.querySelector(".calculator");
 const buttonsContainer = document.querySelector(".buttons");
@@ -23,6 +17,8 @@ const divide = (num1, num2) => num1 / num2;
 // Call the appropriate function depending on operator passed in.
 // If this were in an object, could use computed member access to make this cleaner.
 const operate = (num1, operator, num2) => {
+  num1 = Number(num1);
+  num2 = Number(num2);
   if (operator === "add") return add(num1, num2);
   if (operator === "subtract") return subtract(num1, num2);
   if (operator === "multiply") return multiply(num1, num2);
@@ -45,26 +41,64 @@ buttonsContainer.addEventListener("click", (e) => {
     updateDisplay();
     return;
   }
+  // clear button pressed
   if (dataset.function === "clear") {
     clearDisplay();
+    updateDisplay();
+  }
+
+  if (dataset.function === "allClear") {
+    resetCalcuator();
+    updateDisplay();
+  }
+
+  // an operator pressed
+  if (btn.classList.contains("operator")) {
+    console.log(dataset.function);
+    // Store the first number
+    calculator.num1 = calculator.displayValue;
+    clearDisplay();
+    calculator.operator = dataset.function;
+  }
+
+  if (btn.classList.contains("equals")) {
+    console.log("hmm");
+    calculator.num2 = calculator.displayValue;
+    // need to
+    setDisplayValue(
+      operate(calculator.num1, calculator.operator, calculator.num2)
+    );
+    updateDisplay();
   }
 });
 
+const setDisplayValue = (num) => {
+  calculator.displayValue = num;
+};
 const updateDisplayValue = (num) => {
   calculator.displayValue =
     calculator.displayValue === "0" ? num : (calculator.displayValue += num);
 };
 
-const updateDisplay = (num) => {
+const updateDisplay = () => {
   displayEle.textContent = calculator.displayValue;
 };
 
 const clearDisplay = () => {
   calculator.displayValue = "0";
+};
+
+const init = () => {
+  generateCalculator();
+  resetCalcuator();
   updateDisplay();
 };
 
-const init = function () {
-  generateCalculator();
-  updateDisplay();
+const resetCalcuator = () => {
+  calculator.num1 = null;
+  calculator.num2 = null;
+  calculator.operator = null;
+  calculator.result = null;
+  calculator.displayValue = "0";
+  calculator.firstNum = false;
 };
