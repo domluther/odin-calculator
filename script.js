6f96504d6759efcc56040a4114337f428dfe90c9
dom <35171942+domluther@users.noreply.github.com>
2023-04-14 18:26:04 +0100

    can input & clear values

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const calculator = {
   num2: 0,
   operator: null,
   result: 0,
+  displayValue: "0",
 };
 
 const calculatorEle = document.querySelector(".calculator");
+const buttonsContainer = document.querySelector(".buttons");
+const displayEle = document.querySelector(".display");
 
 // Basic operations
 const add = (num1, num2) => num1 + num2;
@@ -27,4 +30,41 @@ const operate = (num1, operator, num2) => {
   return "Operator not recognised";
 };
 
+// Use later to programmatically generate the calcluator instead of hardcoding it
 const generateCalculator = function () {};
+
+buttonsContainer.addEventListener("click", (e) => {
+  btn = e.target.closest(".button");
+  if (!btn) return;
+
+  // A button has been pressed - if it's a number, append the value to the displayed value
+  const dataset = btn.dataset;
+  if (dataset.function === "num") {
+    console.log(btn);
+    updateDisplayValue(btn.textContent);
+    updateDisplay();
+    return;
+  }
+  if (dataset.function === "clear") {
+    clearDisplay();
+  }
+});
+
+const updateDisplayValue = (num) => {
+  calculator.displayValue =
+    calculator.displayValue === "0" ? num : (calculator.displayValue += num);
+};
+
+const updateDisplay = (num) => {
+  displayEle.textContent = calculator.displayValue;
+};
+
+const clearDisplay = () => {
+  calculator.displayValue = "0";
+  updateDisplay();
+};
+
+const init = function () {
+  generateCalculator();
+  updateDisplay();
+};
